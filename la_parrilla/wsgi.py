4abf678374addc697e0c1f2133082446cc04abc6
JayKearney <indi.mh@gmail.com>
2022-04-19 15:36:14 +0000

    uninstall old django version and added new one, perform migrations again

--- la_parrilla/wsgi.py
+++ la_parrilla/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for la_parrilla_argentina project.
+WSGI config for la_parrilla project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'la_parrilla_argentina.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'la_parrilla.settings')
 
 application = get_wsgi_application()
