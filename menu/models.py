6d54c1cd16ee5cf0d792c9c68962cb3045b48279
JayKearney <indi.mh@gmail.com>
2022-04-20 11:11:09 +0000

    fix meatss error and add slug

--- menu/models.py
+++ menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,8 +11,19 @@ class Meats(models.Model):
     price = models.DecimalField(max_digits=5 , decimal_places=2)
     preparation_time = models.IntegerField()
     image = models.ImageField(upload_to='menu/')
+    slug = models.SlugField(blank=True, null=True)
+
+    def save(self, *args , **kwargs):
+        if not self.slug and self.name :
+            self.slug = slugify(self.name)
+        super(Meats, self).save(*args , **kwargs)
 
     
     def __str__(self):
         return self.name
+
+    
+    class Meta: 
+        verbose_name = 'meat'
+        verbose_name_plural = 'meats'
 
